7c52393b336e3d7aace2d3c7d37c2132be0c4f7a
martinnajjar12 <martin_najjar@yahoo.com>
2020-11-12 20:32:55 +0300

    Create new.html.erb for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action: authenticate_user!, except: [:index]
+  # before_action :authenticate_user!, except: [:index]
   
   def index
     @posts = Post.all
@@ -15,11 +15,12 @@ class PostsController < ApplicationController
       redirect_to posts_path
     else
       render 'new'
+    end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:posts).permit(:title, :body)
   end
 end
